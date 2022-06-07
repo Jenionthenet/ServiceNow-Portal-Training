4580919cc2e4f3bdc82401fbb0d0a93d894e9809
Jenifer Ine <jenifer.s.ine@gmail.com>
2022-06-07 17:43:13 -0400

    finished

--- dev110462/sp_widget/incident filter test/4 Server Script.js
+++ dev110462/sp_widget/incident filter test/4 Server Script.js
@@ -1,1 +1,24 @@
-(function() {  /* populate the 'data' object */  /* e.g., data.table = $sp.getValue('table'); */})();
+(function () {
+
+
+	data.array = [];
+	getIncidents();
+
+	function getIncidents() {
+		var gr = new GlideRecord('incident');
+		gr.setLimit(20);
+		gr.orderByDesc("sys_updated_on")
+		gr.addQuery("active", true);
+		gr.query();
+		while (gr.next()) {
+			var obj = {
+				id: gr.sys_id.toString(),
+				number: gr.number.toString(),
+				state: gr.state.getDisplayValue(),
+				short_description: gr.short_description.toString()
+			};
+			data.array.push(obj);
+		}
+	}
+
+})();
